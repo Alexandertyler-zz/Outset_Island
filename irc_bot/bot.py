8816b669c94dc4a201e875a42d93f577e4791642
Alex Tyler <at@norsecorp.com>
2015-07-01 15:28:31 -0700

    more options

--- irc_bot/bot.py
+++ irc_bot/bot.py
@@ -1,33 +1,94 @@
 import socket
 import time
 import random
+import sys
 
 server = "chat.norse-data.com"
 channel = "#appliance"
 botnick = "shitty_bot-alex"
 
-bad_words = ["fuck", "shit", "cunt", "bitch", "ass", "asshole", "dick", "fucked"] 
+bad_words = ["fuck", "shit", "cunt", "bitch", "ass", "asshole", "dick", "fucked"]
 
+def parse_commands(chan, nick, msg):
+    print chan, nick, msg
+
+    if msg == '.roll':
+        roll(chan, nick)
+
+    if msg == '.webex':
+        webex(chan)
+
+    if msg.startswith(".suggest"):
+        try:
+            suggestion = msg.split('.suggest ')[1]
+            suggest(chan, nick, suggestion)
+        except:
+           sendmsg(chan, "Sorry, please add a space between your command and the suggestion") 
+
+
+    if msg == '.help':
+        print_help(chan)
+
+    if msg == '.kill':
+        suicide(chan)
+     
+    """
+    for word in bad_words:
+        if word in msg:
+            sendmsg(channel, "~~Language Please~~")
+            break
+    """
 
 def ping(response):
     ircsock.send("PONG "+ response+"\n")
     
+
+def introduction(chan):
+    ircsock.send("PRIVMSG " + chan + " :Hello world! I am Alex's work in progress irc_bot.\n")
+    print_help(chan) 
+
+
 def sendmsg(chan, msg):
     ircsock.send("PRIVMSG " + chan + " :" + msg +"\n")
+
 
 def joinchannel(chan):
     print "Joining channel", chan
     ircsock.send("JOIN  " + chan + "\n")
 
+
 def hello(newnick):
     ircsock.send("PRIVMSG " + channel + " :Hello!\n")
+
 
 def roll(chan, nick):
     rand_num = random.randint(1, 6)
     ircsock.send("PRIVMSG " + chan + " :" + nick + " has rolled a " + str(rand_num) + '\n')
 
+
+def webex(chan):
+    ircsock.send("PRIVMSG " + chan + " :User: Appliance Team\n")
+    ircsock.send("PRIVMSG " + chan + " :Password: Darkwatchsm650\n")
+
+
+def suggest(chan, nick, suggestion):
+    ircsock.send("PRIVMSG " + chan + " :Thanks " + nick + " for your suggestion. Alex will be notified\n")
+    with open("irc_suggestions.txt", "a+") as sugg_file:
+        sugg_file.write(suggestion + '\n')
+
+
 def print_help(chan):
-   ircsock.send("PRIVMSG " + chan + " :How can I be of assistance? My current functions include .roll and .help\n") 
+   ircsock.send("PRIVMSG " + chan + " :How can I be of assistance? My current functions include:\n")
+   ircsock.send("PRIVMSG " + chan + " :    .roll - roll a random number between 1 and 6\n")
+   ircsock.send("PRIVMSG " + chan + " :    .webex - get the webex credentials for the appliance team\n")
+   ircsock.send("PRIVMSG " + chan + " :    .suggest - suggest a command you would like and I'll notify alex for you\n")
+   ircsock.send("PRIVMSG " + chan + " :    .help - print this list of commands\n")
+   ircsock.send("PRIVMSG " + chan + " :    .kill - emergency escape from the irc client, use sparingly please\n")
+   ircsock.send("PRIVMSG " + chan + " :I'm still a work in progress so my commands will be updated all the time. Check back later for more options!\n")
+
+def suicide(chan):
+    ircsock.send("PRIVMSG " + chan + " :Goodbye cruel world\n")
+    sys.exit(1)
 
 def verify():
     while 1:
@@ -40,19 +101,24 @@ def verify():
             ping(response)
             return
 
+try:
+    channel = '#' + str(sys.argv[1])
+except:
+    print "No channel specified, defaulting to #appliance"
+
+
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ircsock.connect((server, 6667))
 ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :Developed by Cyberdyne Systems\n")
 ircsock.send("NICK " + botnick +"\n")
 
 verify()
-
 joinchannel(channel)
+introduction(channel)
 
 while 1:
     ircmsg = ircsock.recv(2048)
     ircmsg = ircmsg.strip('\n\r')
-    print(ircmsg)
 
     if ircmsg.find(":Hello "+ botnick) != -1:
         hello()
@@ -62,22 +128,8 @@ while 1:
         ping(response)
 
     if ircmsg.find(' PRIVMSG ') != 1:
-        nick = ircmsg.split('!')[0][1:]
         chan = ircmsg.split(' PRIVMSG ')[-1].split(' :')[0]
+        nick = ircmsg.split('!')[0][1:]
         msg = ircmsg.split(' PRIVMSG ')[-1].split(' :')[1]
-        print nick, chan, msg
         
-        if msg == '.roll':
-            roll(chan, nick)
-
-        if msg == '.help':
-            print_help(chan)
-
-        
-        
-        """
-        for word in bad_words:
-            if word in msg:
-                sendmsg(channel, "~~Language Please~~")
-                break
-        """
+        parse_commands(chan, nick, msg)
